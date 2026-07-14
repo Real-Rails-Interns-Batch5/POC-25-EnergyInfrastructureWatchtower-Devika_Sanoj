bcf75bccf0c5d6d88caf03188ddb9f70ec100611
Devika-sanoj <devikasanoj24@gmail.com>
2026-07-14 12:59:51 +0530

    chore: dockerize frontend and backend and fix api url configuration

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,6 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://backend:8000";
 
 export async function fetchPlants() {
   const res = await fetch(`${BASE_URL}/api/plants/`);
